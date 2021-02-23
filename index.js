3f0c9e9a45e6baac8a32d54ff843d94b874d9cff
parisa-06 <75797030+parisa-06@users.noreply.github.com>
2021-02-23 13:36:46 +0330

    Bug fixes json

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-function myFunctionTable(){
-    var Table={
-   
-        "firstName":"parisa",
-        "lastName":"zangiabadi",
-        "age" : "23"
-  
-};
-console.log(Table);
+var myTable = {
+    "employees": [{
+            "firstName": "parisa",
+            "lastName": "zangiabadi",
+            "age": "23"
 
-var content = "<table>"
-for(i=0; i<3; i++){
-    content += '<tr><td>' + 'result ' +  i + '</td></tr>';
-}
-content += "</table>"
+        },
+        {
+            "firstName": "psa",
+            "lastName": "zabadi",
+            "age": "2"
 
-$('#demo').append(content);
+        }
+    ]
 
 };
+
+var count = Object.keys(myTable.employees).length;
+console.log(count);
+
+console.log(myTable);
+
+
+var number_of_rows = count;
+var number_of_cols = count;
+
+var mytable = "<table border=\"1\"> <tr> <th>row</th> <th>firstName</th> <th>lastName</th> <th>age</th> </tr>";
+
+for (var i = 1; i <= number_of_rows; i++) {
+    console.log("i: ", i);
+    mytable += '<tr> <td> ' + i + '</td>';
+    for (x in myTable.employees[i - 1]) {
+
+        mytable += '<td> ' + myTable.employees[i - 1][x] + ' </td>';
+        console.log(myTable.employees[i - 1][x]);
+    };
+    mytable += '<tr>';
+};
+
+mytable += '</table>';
+$('#demo').append(mytable);
